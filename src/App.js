d4193b506e25feaaa275c8157acd103f8abc3e79
sheready <csstlw@gmail.com>
2023-01-14 00:52:59 +0300

    lift state up from newexpense app to app.js

--- src/App.js
+++ src/App.js
@@ -23,9 +23,13 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  }
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense  onAddExpense={addExpenseHandler} />
       <Expenses data={expenses} />
       
     </div>
