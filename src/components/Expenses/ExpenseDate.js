be6f2c3a34a1d7f0c7ac3a432d06f9ac396f00ac
sheready <csstlw@gmail.com>
2023-01-14 11:14:16 +0300

    debug to ensure the date is available

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,9 +1,9 @@
 import '../UI/ExpenseDate.css';
 
 function ExpenseDate(props){
-    const month =  props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
+    const month =  props.date?.toLocaleString('en-US', {month: 'long'});
+    const day = props.date?.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date?.getFullYear();
 
     return (
         <div className='expense-date'>
