0e7a592878c54d971b1c372dc7bde0645836d941
sheready <csstlw@gmail.com>
2023-02-14 10:14:25 +0300

    edit addExpenseHandler to handle state effectively

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ const initial_expenses = [
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', 
+    title: 'New TV', 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) 
+  },
   {
     id: 'e3',
     title: 'Car Insurance',
