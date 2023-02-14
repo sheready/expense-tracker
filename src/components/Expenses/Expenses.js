4e5364b6d9ca708125f1c8d45ab3f5ed4db6473e
sheready <csstlw@gmail.com>
2023-02-14 10:12:40 +0300

    remove redundant expense list functionality from expense item

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpenseItem from './ExpenseItem'
 import Card from './Card'
 import '../UI/Expenses.css'
 import Expensesfilter from './Expensesfilter'
+import ExpensesList from './ExpensesList'
 
 const Expenses = (props) => {
   const[filteredYear, setFilteredYear] = useState('2020');
@@ -14,25 +15,11 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <h3>No expenses found for that year</h3>;
-
-  if(filteredExpenses.length > 0){
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem 
-        key = {expense.id}
-        title = {expense.title} 
-        amount = {expense.amount}
-        date = {expense.date}
-        />
-      )
-    )
-  }
-
   return (
     <div>
       <Card className='expenses'>
         <Expensesfilter selected={filteredYear} onSaveYear={getYearvalue} />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   )
