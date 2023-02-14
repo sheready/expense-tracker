d3f5a291a2a736459e1afabc005827ef026eba47
sheready <csstlw@gmail.com>
2023-02-14 10:11:51 +0300

    remove redundant expense list functionality from expense item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,23 +4,18 @@ import Card from './Card';
 import '../UI/ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // const [title, setTitle] = useState(props.title);
-
-
-  // const clickHandler = () => {
-  //   setTitle('Updated title');
-  //   console.log(title);
-  // }
 
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date}/>
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>Kshs.{props.amount}</div>
-      </div>
-      {/* <button onClick={clickHandler}>Change status</button> */}
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date}/>
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>Kshs.{props.amount}</div>
+        </div>
+        {/* <button onClick={clickHandler}>Change status</button> */}
+      </Card>
+    </li>
   )
 }
 
