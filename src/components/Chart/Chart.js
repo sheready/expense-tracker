c8f9678dedcd1d73c773aff955ad77fc2b9023ee
sheready <csstlw@gmail.com>
2023-02-16 10:24:20 +0300

    add ChartBar component and pass down properties, iterate the dataPoints props and get the maximum expense value in a month

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,9 +3,24 @@ import '../Chart/Chart.css'
 import ChartBar from './ChartBar'
 
 const Chart = (props) => {
+  // calculate max value of the month
+  const dataPointsValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  //pull out all array elements from dataPointsValues as stand alone arguments
+  const totalMaximum = Math.max(...dataPointsValues);
+
   return (
     <div className='chart'>
-        {props.dataPoints.map(dataPoint => <ChartBar value={dataPoint.value}/>)}
+        {props.dataPoints.map(dataPoint => 
+        // pass value to ChartBar that will be rendered
+        // in this case its the date points value property
+        // ensure it passes a maximum value of the value
+        // pass in the label prop to reflect each month
+        <ChartBar 
+          key={dataPoint.label}
+          value={dataPoint.value} 
+          maxValue={totalMaximum}
+          label={dataPoint.label} 
+        />)}
 
     </div>
   )
