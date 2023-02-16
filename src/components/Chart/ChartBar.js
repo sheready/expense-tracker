00eca0cc68df8977344ce8dad29636ec73d7da03
sheready <csstlw@gmail.com>
2023-02-16 10:26:59 +0300

    add dynamic height of the expense bar

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import '../Chart/ChartBar.css'
 
-const ChartBar = () => {
+const ChartBar = (props) => {
+  let barFillHeight = '0%';
+  // to  get the % of how much the bar should be filled using css height property
+  if(props.maxValue > 0){
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%' // to convert it to a string
+  }
   return (
-    <div>ChartBar</div>
+    <div className='chart-bar'>
+      <div className='chart-bar__inner'>
+        {/* make the height style dynamic */}
+        {/* we add a 2nd curly bracket because we are passing a dynamic js object */}
+        <div className='chart-bar__fill' style={{height: barFillHeight}}></div>
+      </div>
+      <div className='chart-bar__label'>{props.label}</div>
+    </div>
   )
 }
 
